a7f248891aab7d24c5acfa6d648a2b240e25b849
Ethan Garofolo <ethan@suchsoftware.com>
2017-10-06 11:44:50 -0600

    Remove rule about JSX curlies because Prettier

--- index.js
+++ index.js
@@ -1,52 +1,34 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "commonjs": true,
-        "es6": true,
-        "node": true
+  env: {
+    browser: true,
+    commonjs: true,
+    es6: true,
+    node: true
+  },
+  extends: ["eslint:recommended", "plugin:react/recommended", "airbnb"],
+  parser: "babel-eslint",
+  parserOptions: {
+    ecmaFeatures: {
+      experimentalObjectRestSpread: true,
+      jsx: true
     },
-    "extends": [ "eslint:recommended", "plugin:react/recommended", "airbnb" ],
-    "parser": "babel-eslint",
-    "parserOptions": {
-        "ecmaFeatures": {
-            "experimentalObjectRestSpread": true,
-            "jsx": true
-        },
-        "sourceType": "module"
-    },
-    "plugins": [
-        "react"
-    ],
-    "rules": {
-        "array-bracket-spacing": [ "off" ],
-        "arrow-parens": [ "error", "as-needed" ],
-        "comma-dangle": [
-            "error",
-            "always-multiline"
-        ],
-        "comma-style": [
-            "error",
-            "last"
-        ],
-        "import/prefer-default-export": [ "off" ],
-        "max-len": [ "error", 80 ],
-        "no-console": "error",
-        "no-debugger": 2,
-        "no-use-before-define": [ "error", "nofunc" ],
-        "react/jsx-curly-spacing": [ "error", "always" ],
-        "react/jsx-filename-extension": [
-          "error",
-          { "extensions": [".js", ".jsx"] }
-        ],
-        "react/jsx-wrap-multilines": [ "off" ],
-        "react/no-unused-prop-types": [
-            "error",
-            { "skipShapeProps": true }
-        ],
-        "semi": [
-            "error",
-            "never"
-        ],
-        "template-curly-spacing": [ "off" ]
-    }
+    sourceType: "module"
+  },
+  plugins: ["react"],
+  rules: {
+    "array-bracket-spacing": ["off"],
+    "arrow-parens": ["error", "as-needed"],
+    "comma-dangle": ["error", "always-multiline"],
+    "comma-style": ["error", "last"],
+    "import/prefer-default-export": ["off"],
+    "max-len": ["error", 80],
+    "no-console": "error",
+    "no-debugger": 2,
+    "no-use-before-define": ["error", "nofunc"],
+    "react/jsx-filename-extension": ["error", { extensions: [".js", ".jsx"] }],
+    "react/jsx-wrap-multilines": ["off"],
+    "react/no-unused-prop-types": ["error", { skipShapeProps: true }],
+    semi: ["error", "never"],
+    "template-curly-spacing": ["off"]
+  }
 };
